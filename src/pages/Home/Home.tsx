a9b67759fef4303614dd2974c46753fc85b1681e
ironhornet <tarmyrka@gmail.com>
2023-09-12 22:25:58 +0300

    add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,11 @@
+import { Filter } from '../../components/Filter/Filter';
+import { ListOfItems } from '../../components/ListOfItems/ListOfItems';
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Filter />
+      <ListOfItems />;
+    </div>
+  );
+};
